b67957931d6e23fb6bb117e93ce0e96ce4442381
Tudor Andrei Oprea <t.oprea1@student.tue.nl>
2026-03-12 17:41:43 +0100

    Add fetch key from file and save key to file functionality.

--- src/key_generator.rs
+++ src/key_generator.rs
@@ -1,5 +1,6 @@
 use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
 use rsa::pkcs8::EncodePrivateKey;
+use rsa::pkcs8::DecodePrivateKey;
 use rsa::pkcs8::LineEnding;
 
 pub fn generate_key() -> (RsaPublicKey, RsaPrivateKey){
@@ -9,4 +10,11 @@ pub fn generate_key() -> (RsaPublicKey, RsaPrivateKey){
     let public_key = RsaPublicKey::from(&private_key);
 
     (public_key, private_key)
+}
+fn fetch_key_from_file(path : &str) -> rsa::RsaPrivateKey {
+    rsa::RsaPrivateKey::read_pkcs8_der_file(path).expect("Could not read private key")
+}
+
+fn save_key_to_file(path : &str, private_key : &rsa::RsaPrivateKey) {
+    private_key.write_pkcs8_der_file(path).expect("Could not write private key as PEM")
 }
