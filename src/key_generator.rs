abd6ed86e08a618b044664305d319bd40d702dc2
Beni <b.levi@student.tue.nl>
2026-03-12 17:33:55 +0100

    Key generation interface defined and function done.

--- src/key_generator.rs
+++ src/key_generator.rs
@@ -2,20 +2,11 @@ use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
 use rsa::pkcs8::EncodePrivateKey;
 use rsa::pkcs8::LineEnding;
 
-pub fn generate_key() {
+pub fn generate_key() -> (RsaPublicKey, RsaPrivateKey){
     let mut rng = rand::rng();
     let bits = 2048;
     let private_key = RsaPrivateKey::new(&mut rng, bits).expect("Failed to generate key.");
     let public_key = RsaPublicKey::from(&private_key);
 
-    let data = b"Hello world!";
-    let enc_data = public_key.encrypt(&mut rng, Pkcs1v15Encrypt, &data[..]).expect("failed to encrypt");
-    assert_ne!(&data[..], &enc_data[..]);
-
-    let dec_data = private_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("failed to decrypt");
-    assert_eq!(&data[..], &dec_data[..]);
-
-    let decrypted_message = String::from_utf8(dec_data).unwrap();
-    println!("Decrypted message: {}", decrypted_message);
+    (public_key, private_key)
 }
-
