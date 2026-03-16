9ba52b70d8c72adfae6e1f47d839bb13c4143ce4
Beni <b.levi@student.tue.nl>
2026-03-16 15:39:44 +0100

    fix(code-quality): Added code comments for hybrid_encryption.rs.

--- src/hybrid_encryption.rs
+++ src/hybrid_encryption.rs
@@ -4,12 +4,20 @@ use crate::aes_service::{decrypt_aes, encrypt_aes};
 use crate::rsa_service::{decrypt_rsa, encrypt_rsa};
 use crate::helper::AesNonce;
 
-pub fn encrypt_hybrid(data: &[u8], pub_key : &RsaPublicKey) -> (Vec<u8>, AesNonce, Vec<u8>){
-    let (cipher_text, nonce, key) = encrypt_aes(data);
+/// Encrypts the given plain text with AES-GCM and RSA to communicate the text through unencrypted channels.
+/// The text is first encrypted with AES-GCM, then the AES key is encrypted with recipients public RSA key.
+/// ### Returns:
+/// Cipher_text, AES Nonce, Encrypted Key
+pub fn encrypt_hybrid(plain_text: &[u8], pub_key : &RsaPublicKey) -> (Vec<u8>, AesNonce, Vec<u8>){
+    let (cipher_text, nonce, key) = encrypt_aes(plain_text);
     let encrypted_key = encrypt_rsa(&key, pub_key);
     (cipher_text, nonce, encrypted_key)
 }
 
+/// Decrypts the given cipher text.
+/// The encrypted key is first decrypted with recipients private key, then the cipher text key is decrypted with AES-GCM.
+/// ### Returns:
+/// Plain Text
 pub fn decrypt_hybrid(cipher_text: &[u8], nonce:AesNonce, encrypted_key: &[u8], priv_key: &RsaPrivateKey) -> Vec<u8> {
     let key_vector = decrypt_rsa(&encrypted_key, priv_key);
     let key = Key::<Aes256Gcm>::from_slice(&key_vector);
