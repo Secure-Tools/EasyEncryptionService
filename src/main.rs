45a4f7bfdd0bab725bb276018828a9cbddd67d2d
Beni <b.levi@student.tue.nl>
2026-03-17 21:28:17 +0100

    feat(cli): Added CLI commands generate, encrypt, and decrypt.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use crate::key_generator::generate_rsa_key;
+use crate::key_generator::{fetch_key_from_file, generate_rsa_key, save_key_to_file};
 use crate::rsa_service::{encrypt_rsa, decrypt_rsa};
 use crate::helper::u8_to_string;
 use crate::aes_service::{encrypt_aes, decrypt_aes};
 use crate::hybrid_encryption::{decrypt_hybrid, encrypt_hybrid};
-use crate::packer::{pack_message, unpack_message};
+use crate::packer::{pack_message, pack_public_key, unpack_message, unpack_public_key};
+use clap::{Parser, Subcommand};
 
 pub mod key_generator;
 pub mod helper;
@@ -12,33 +13,56 @@ pub mod aes_service;
 pub mod hybrid_encryption;
 pub mod packer;
 
+#[derive(Parser)]
+#[command(name = "ees", about = "Easy encryption service CLI tool")]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Generate,
+    /// Encrypts a text using a base62 encoded RSA public key.
+    Encrypt {
+        #[arg(short, long)]
+        text: String,
+        // Base62 encoded RSA public key
+        #[arg(short, long)]
+        pub_key: String
+    },
+    /// Decrypts a text using your private key.
+    Decrypt {
+        #[arg(short, long)]
+        text: String,
+        /// Path to .pcks8 private key file (default: private_key.pkcs8)
+        #[arg(short, long, default_value = "private_key.pkcs8")]
+        key_file: String,
+    }
+}
 fn main() {
-    let (pub_key, priv_key) = generate_rsa_key();
-    let message = b"Let's meet at 4PM!";
-    let cipher_message = encrypt_rsa(message, &pub_key);
-    let decoded_message = decrypt_rsa(&cipher_message, &priv_key);
-    let extracted_message = u8_to_string(decoded_message);
+    let cli = Cli::parse();
 
-    println!("RSA extracted: {extracted_message}");
-
-    let plain_text = b"Let's meet at 5PM!";
-    let (cipher_text, nonce, key) = encrypt_aes(plain_text);
-    let extracted_text = decrypt_aes(&cipher_text, nonce , &key);
-
-    println!("AES extracted: {}", u8_to_string(extracted_text));
-
-    let plain_text = b"Let's meet at 6PM!";
-    let (cipher_text, nonce, enc_key) = encrypt_hybrid(plain_text, &pub_key);
-    let extracted_text = decrypt_hybrid(&cipher_text, nonce, &enc_key, &priv_key);
-
-    println!("Hybrid extracted: {}", u8_to_string(extracted_text));
-
-    let plain_text = b"Let's meet at 7PM!";
-    let (cipher_text, nonce, enc_key) = encrypt_hybrid(plain_text, &pub_key);
-    let packed_data = pack_message(&cipher_text, nonce, &enc_key);
-    println!("Packed data: {}", packed_data);
-    let (cipher_text, nonce, enc_key) = unpack_message(&packed_data);
-    let extracted_text = decrypt_hybrid(&cipher_text, nonce, &enc_key, &priv_key);
-
-    println!("Packed extracted: {}", u8_to_string(extracted_text));
+    match cli.command {
+        Command::Generate => {
+            let (pub_key, priv_key) = generate_rsa_key();
+            save_key_to_file("private_key.pkcs8", &priv_key);
+            println!("\nRSA key generation successful! \n \
+            Private key has been saved to private_key.pkcs8. \n\
+            Public key: {}", pack_public_key(&pub_key));
+        }
+        Command::Encrypt {text, pub_key} => {
+            let pub_key = pub_key.trim();
+            let pub_key = unpack_public_key(pub_key);
+            let (cipher_text, nonce, enc_key) = encrypt_hybrid(text.as_bytes(), &pub_key);
+            let packed_text = pack_message(&cipher_text, nonce, &enc_key);
+            println!("\nEncrypted message: {}", packed_text);
+        }
+        Command::Decrypt {text, key_file} => {
+            let key_file = key_file.trim();
+            let (cipher_text, nonce, enc_key) = unpack_message(&text);
+            let extracted_text = decrypt_hybrid(&cipher_text, nonce, &enc_key , &fetch_key_from_file(&key_file));
+            println!("\nDecrypted message: {}", u8_to_string(extracted_text));
+        }
+    }
 }
--- src/packer.rs
+++ src/packer.rs
@@ -8,12 +8,13 @@ pub fn pack_message(ciphertext: &[u8], nonce: AesNonce, enc_key: &[u8]) -> Strin
     let nonce_code = encode(&nonce);
     let enc_key_code = encode(enc_key);
 
-    let packed_cipher = format!("{cipher_code}|{nonce_code}|{enc_key_code}");
+    let packed_cipher = format!("{cipher_code}.{nonce_code}.{enc_key_code}");
     packed_cipher
 }
 
 pub fn unpack_message(packed_cipher: &str) -> (Vec<u8>, AesNonce,  Vec<u8>) {
-    let mut split = packed_cipher.split('|');
+    let packed_cipher = packed_cipher.trim();
+    let mut split = packed_cipher.split('.');
     let ciphertext = decode(split.next().unwrap()).expect("Failed to decode.");
     let nonce = decode(split.next().unwrap()).expect("Failed to decode.");
     let enc_key = decode(split.next().unwrap()).expect("Failed to decode.");
@@ -25,7 +26,8 @@ pub fn pack_public_key(pub_key:&RsaPublicKey) -> String {
     encode(pub_key.to_public_key_der().expect("Failed to der").as_bytes())
 }
 
-pub fn unpack_public_key(pub_key_packed :String) -> RsaPublicKey {
+pub fn unpack_public_key(pub_key_packed :&str) -> RsaPublicKey {
+    let pub_key_packed = pub_key_packed.trim();
     let der_bytes = decode(&pub_key_packed).expect("Failed to decode");
     RsaPublicKey::from_public_key_der(&der_bytes).expect("Failed to generate public key from bytes")
 }
@@ -51,7 +53,7 @@ mod tests {
     fn test_pack_for_public_key() {
         let (pub_key, _priv_key) = generate_rsa_key();
         let encoded_key = pack_public_key(&pub_key);
-        let decoded_key = unpack_public_key(encoded_key);
+        let decoded_key = unpack_public_key(&encoded_key);
         assert_eq!(pub_key,decoded_key);
     }
 
