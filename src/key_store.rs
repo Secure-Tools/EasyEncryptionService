84bdac2b4516af0bf4e530bc4ac88d3f9b701635
Beni Levi <109583393+benilevi05@users.noreply.github.com>
2026-03-31 11:54:11 +0300

    Feature: Keyring (#13)
    * feature(key_storage): Added a store() method for public key storage with JSON.
    
    * fix(key_storage): Added trim in store() method to delete outer spaces.
    
    * feature(key_storage): Modified key_store.rs so it adds to the file instead of replacing the file when store() is called.
    
    * feature(key_storage): Added list and get functionality to keyring.

---
+++ src/key_store.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+use std::fs;
+use serde::{Deserialize, Serialize};
+use std::path::Path;
+use serde_json;
+
+#[derive(Serialize, Deserialize, Default)]
+struct Keyring {
+    contacts: HashMap<String, String>
+}
+
+pub fn store(name: String, b62_encoded_key: String) {
+    let path = "keyring.json";
+    let mut keyring : Keyring = get_keyring(path);
+
+    keyring.contacts.insert(
+        name.trim().to_string(), b62_encoded_key.trim().to_string()
+    );
+
+    let file = fs::File::create("keyring.json").expect("File couldnt be read/created.");
+    serde_json::to_writer_pretty(file, &keyring).expect("Keyring failed to write.");
+}
+
+/// Given a name, returns the corresponding encoded public key in the keyring.
+pub fn get_key(name:&str) -> Option<String> {
+    let keyring: Keyring = get_keyring("keyring.json");
+    keyring.contacts.get(name).cloned()
+}
+/// Prints all the contacts.
+pub fn list_contacts() {
+    let keyring: Keyring = get_keyring("keyring.json");
+    for name in keyring.contacts.keys() {
+        println!("{}", name);
+    }
+}
+
+fn get_keyring(path: &str) -> Keyring {
+    if Path::new(path).exists() {
+        let contents = fs::read_to_string(path).expect("Failed to read");
+        serde_json::from_str(&contents).expect("Failed to create json from string.")
+    } else {
+        Keyring::default()
+    }
+}
